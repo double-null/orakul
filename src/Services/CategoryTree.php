232d9f3d00e942dbaa79f830ef967cdb493caaea
double-null <double-null@yandex.com>
2022-08-14 20:26:30 +0300

    Category Tree

---
+++ src/Services/CategoryTree.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Orakul\Services;
+use Orakul\Models\Category;
+
+class CategoryTree
+{
+    /**
+     * Строит дерево категорий
+     * @return array
+     */
+    public static function build()
+    {
+        // TODO: на текущий момент формируется 2 уровня
+        // допилить до бесконечного уровня
+        // P.S: Adjacency list
+        $output = [];
+        $categories = Category::getAll();
+        foreach ($categories as $category) {
+            $parent = (int)$category['parent'];
+            if ($parent) {
+                $output[$parent]['childs'][] = $category;
+            } else {
+                // категории 0 уровня
+                $childs = $output[$category['id']]['childs'] ?? [];
+                $output[$category['id']] = $category;
+                $output[$category['id']]['childs'] = $childs;
+            }
+        }
+        return $output;
+    }
+}
